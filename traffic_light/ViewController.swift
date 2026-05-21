529a1ca24b8fdfff26f25fb5de14c78cfc54a661
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2026-05-21 12:16:08 +0300

    Initial Commit

---
+++ traffic_light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic_light
+//
+//  Created by Анастасія on 21.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
