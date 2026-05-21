24bc5d5475982f62f0b144787d4256abd0ce0c92
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2026-05-21 13:01:14 +0300

    feat: add simple start button

--- traffic_light/ViewController.swift
+++ traffic_light/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  traffic_light
-//
-//  Created by Анастасія on 21.05.2026.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .white
+        
+        self.button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
-
+    @objc private func startButtonTapped() {
+        print("button tapped")
+    }
 }
 
